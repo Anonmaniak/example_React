11e2fd4b061df38ebdb55f2abe54c406e5c3f805
Rupesh <mallarupesh>
2023-07-29 21:46:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import FunctionalComp from './Components/FunctionalComp';
+import {ClassComp, ClassComp1} from './Components/ClassComp';
+import Click from './Components/Click';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Javatpoint</h1>
+      <FunctionalComp></FunctionalComp>
+      <ClassComp></ClassComp>
+      <ClassComp1></ClassComp1>
+      <Click></Click>
     </div>
   );
 }
---
+++ src/Components/Click.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+export class Click extends Component{
+    render(){
+        return(
+            <div>
+            <button>Click 1 times</button>
+            </div>
+        );
+    }
+}
+
+export default Click;
